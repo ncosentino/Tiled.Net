bff59c15bb5510bdf97af189ae9ed58cf1416098
ncosentino <n.b.cosentino@gmail.com>
2021-03-18 15:27:24 -0700

    New csproj Format

--- Tiled.Net/Properties/AssemblyInfo.cs
+++ Tiled.Net/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Tiled.Net")]
-[assembly: AssemblyDescription("A C# library for parsing Tiled Map Editor TMX files.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Nick Cosentino")]
-[assembly: AssemblyProduct("Tiled.Net")]
-[assembly: AssemblyCopyright("Copyright © Nick Cosentino 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -20,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("918ec804-6d7a-4438-8715-4c57f1875b9d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
